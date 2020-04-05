84b8bcdb7010cf82d833447fdb57022535279941
padda76 <odd.hartvigsen@gmail.com>
2020-04-05 18:18:17 +0200

    Routing and semanticUI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'semantic-ui-react'
+
+import 'semantic-ui-css/semantic.min.css'
+import './App.css'
+
+import MenuBar from './components/MenuBar'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 function App() {
   return (
-    <div><h1>Hi World</h1></div>
+    <Router>
+      <Container>
+        <MenuBar/>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/register' component={Register}/>
+      </Container>
+    </Router>
   );
 }
 
