ccbb2391cf822f7016a1fc4bce11057a6a009de8
padda76 <odd.hartvigsen@gmail.com>
2020-04-12 21:51:39 +0200

    added createPost errorhandling + transition

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { useQuery } from '@apollo/react-hooks'
-import { Grid } from 'semantic-ui-react'
+import { Grid, Transition } from 'semantic-ui-react'
 import { AuthContext } from '../context/auth'
 import PostForm from '../components/PostForm'
 import PostCard from '../components/PostCard'
@@ -28,11 +28,13 @@ const Home = () => {
                 {loading ? (
                     <h1>Loading posts..</h1>
                 ) : (
-                    posts && posts.map(post => (
-                        <Grid.Column key={post.id} style={{ marginBottom: 20}}>
-                            <PostCard post={post}/>
-                        </Grid.Column>
-                    ))
+                    <Transition.Group>
+                        {posts && posts.map(post => (
+                            <Grid.Column key={post.id} style={{ marginBottom: 20}}>
+                                <PostCard post={post}/>
+                            </Grid.Column>
+                        ))}
+                    </Transition.Group>
                 )}
             </Grid.Row>
         </Grid>
